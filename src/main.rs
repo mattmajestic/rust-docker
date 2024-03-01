1e76ea2c1d0e98a674dfcea4d7fc64e309fa8d47
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2024-03-01 23:18:19 +0000

    basic index.html

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 #[macro_use] extern crate rocket;
-extern crate comrak;
 
-use comrak::{markdown_to_html, ComrakOptions};
-use rocket::response::content;
-use std::fs;
-
-#[get("/")]
-fn index() -> content::Html<String> {
-    let readme = fs::read_to_string("README.md").expect("Unable to read README.md");
-    let html = markdown_to_html(&readme, &ComrakOptions::default());
-    content::Html(html)
-}
+use rocket::fs::FileServer;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", FileServer::from("static"))
 }
