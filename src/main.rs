ad8d6d3e33857165ecae21964d3aa20c51346478
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2024-03-01 22:46:53 +0000

    render readme for app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[macro_use] extern crate rocket;
+extern crate comrak;
+
+use comrak::{markdown_to_html, ComrakOptions};
+use rocket::response::content;
+use std::fs;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> content::Html<String> {
+    let readme = fs::read_to_string("README.md").expect("Unable to read README.md");
+    let html = markdown_to_html(&readme, &ComrakOptions::default());
+    content::Html(html)
 }
 
 #[launch]
